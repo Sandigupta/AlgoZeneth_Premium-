bc664f4940447ad08925fc659e406d7c6f9ade45
Sandigupta <123671335+Sandigupta@users.noreply.github.com>
2024-08-14 23:59:47 +0530

    Add files via upload

---
+++ Two Pointers/Maximum_water_collected_in_histogram.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int histogram_arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> histogram_arr[i];
+    }
+    
+    
+}
